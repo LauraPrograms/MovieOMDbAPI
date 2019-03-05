a99579a556e55689142f299dda36d8311c824dea
Laura <Lhuiefiction@gmail.com>
2019-03-04 19:24:04 -0500

    First commit

---
+++ MovieOMdbAPI/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using MovieOMdbAPI.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieOMdbAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult GetData(string Search)
+        {
+
+            HttpWebRequest request = WebRequest.CreateHttp("http://www.omdbapi.com/?s=" + Search + "&apikey=459c139");
+            //HttpWebRequest request = WebRequest.CreateHttp("http://www.omdbapi.com/?t=" + Search + "&apikey=459c139");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string data = rd.ReadToEnd();
+            JObject MoviesJson = JObject.Parse(data);
+            List<Movies> foundMovies = new List<Movies>();
+            if (MoviesJson["Response"].ToString() == "True")
+            {
+                for (int i = 0; i < MoviesJson["Search"].Count(); i++)
+                {
+                    if (MoviesJson["Search"][i]["Type"].ToString() == "movie")
+                    {
+                        string title = MoviesJson["Search"][i]["Title"].ToString();
+                        string year = MoviesJson["Search"][i]["Year"].ToString();
+                        string poster = MoviesJson["Search"][i]["Poster"].ToString();
+                        Movies m = new Movies(title, year, poster);
+                        foundMovies.Add(m);
+                    }
+                }
+                ViewBag.Results = foundMovies;
+                return View(foundMovies);
+            }
+
+            return View();
+        }
+    }
+}
---
+++ MovieOMdbAPI/Models/Movies.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieOMdbAPI.Models
+{
+    public class Movies
+    {
+        
+        public string Title { get; set; }
+        
+        public int Year { get; set; }
+        public string Poster { get; set; }
+        
+        public Movies()
+        {
+
+        }
+        public Movies(string title, string year, string poster)
+        {
+            
+            Title = title;
+           
+            Year = int.Parse(year);
+           
+            Poster = poster;
+        }
+
+    }
+}
